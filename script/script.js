3e98d0024723746996d107259277229ed9b6e316
mdaffamahendra <daffa123mahendra@gmail.com>
2025-02-03 09:57:52 +0700

    perbaikan website

--- script/script.js
+++ script/script.js
@@ -1,23 +1,32 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzYw4YklOXDiZXABGy9dN91-HOUt4MjdNrHk-9sl7i24MJecVc3jOOD7c43jz8LeFmG/exec';
-const form = document.forms['contact-portofolio'];
-const btnSend = document.querySelector('.btn-send');
-const btnLoading = document.querySelector('.btn-loading');
-const myAlert = document.querySelector('.alert');
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbzYw4YklOXDiZXABGy9dN91-HOUt4MjdNrHk-9sl7i24MJecVc3jOOD7c43jz8LeFmG/exec';
+// const form = document.forms['contact-portofolio'];
+// const btnSend = document.querySelector('.btn-send');
+// const btnLoading = document.querySelector('.btn-loading');
+// const myAlert = document.querySelector('.alert');
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  // Ketika tombol submit di klik
-  btnLoading.classList.toggle('d-none');
-  btnSend.classList.toggle('d-none');
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      btnLoading.classList.toggle('d-none');
-      btnSend.classList.toggle('d-none');
-      myAlert.classList.toggle('d-none');
-      form.reset();
-      console.log('Success!', response); })
-    .catch(error => console.error('Error!', error.message))
-})
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   // Ketika tombol submit di klik
+//   btnLoading.classList.toggle('d-none');
+//   btnSend.classList.toggle('d-none');
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//       btnLoading.classList.toggle('d-none');
+//       btnSend.classList.toggle('d-none');
+//       myAlert.classList.toggle('d-none');
+//       form.reset();
+//       console.log('Success!', response); })
+//     .catch(error => console.error('Error!', error.message))
+// })
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let message = document.getElementById("message").value;
+  let phoneNumber = "6288809188740"; // Nomor WhatsApp tanpa tanda +
+  let whatsappURL = `https://wa.me/${phoneNumber}?text=Halo,%20saya%20${encodeURIComponent(name)}.%20${encodeURIComponent(message)}`;
+  window.open(whatsappURL, "_blank");
+});
 
 AOS.init();
 gsap.registerPlugin(TextPlugin);
